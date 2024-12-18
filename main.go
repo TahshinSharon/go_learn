2cc142a4ec9c0525a2963a78fbbf8108e666997d
TahshinSharon <atsharon88@gmail.com>
2024-12-18 18:21:14 +0600

    This

--- main.go
+++ main.go
@@ -9,23 +9,25 @@ func main() {
 	var remainingTickets uint = conferenceTicket
 	fmt.Println("We have total of", conferenceTicket, "Tickets and", remainingTickets, "are still available.")
 	fmt.Println("Get your tickets here to attend")
-	var username string
-	var userTicket uint
-	var email string
-	fmt.Println("Enter Your Username:")
-	fmt.Scan(&username)
-	fmt.Println("Enter your Email address:")
-	fmt.Scan(&email)
-	fmt.Println("Enter Your Ticket:")
-	fmt.Scan(&userTicket)
-	remainingTickets = remainingTickets - userTicket
+	for {
+		var username string
+		var userTicket uint
+		var email string
+		fmt.Println("Enter Your Username:")
+		fmt.Scan(&username)
+		fmt.Println("Enter your Email address:")
+		fmt.Scan(&email)
+		fmt.Println("Enter Your Ticket:")
+		fmt.Scan(&userTicket)
+		remainingTickets = remainingTickets - userTicket
 
-	//
+		//
 
-	//
+		//
 
-	//
+		//
 
-	fmt.Printf("Thank you %v for booking %v tickets.You will receive a confirmation email at %v\n", username, userTicket, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferencename)
+		fmt.Printf("Thank you %v for booking %v tickets.You will receive a confirmation email at %v\n", username, userTicket, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferencename)
+	}
 }
